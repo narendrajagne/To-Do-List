ce46cff53fb62adc0afe57b411160d3a00a4db0c
narendrajagne <nkjagne1@gmail.com>
2025-03-17 19:03:41 +0530

    sample commit

---
+++ To-Do-List/To-Do-List/MVVM/ViewModel/SwiftUIQuesAnswer.swift
@@ -0,0 +1,30 @@
+//
+//  SwiftUIQuesAnswer.swift
+//  To-Do-List
+//
+//  Created by Mobile2 on 13/03/25.
+//
+
+import Foundation
+
+// SwiftUI Question and Answer
+
+/*
+
+ ////// Basics of SwiftUI
+ 
+ 1. What is SwiftUI ?
+ Answer:
+ - SwiftUI is Apple's declarative framework for building user interfaces across all Apple platforms with less code and improved interoperability.
+ 
+ 2. How does SwiftUI differ from UIKit?
+ Answer:
+ - Declarative vs Imperative UI.
+ - Composable views in SwiftUI, storybaord reliance in UIKit.
+ - Reactive state binding in SwiftUI, traditional data flow in UIKIt.
+ 
+ 3. What platfroms does SwiftUI support?
+ Answer:
+ 
+ 
+*/
