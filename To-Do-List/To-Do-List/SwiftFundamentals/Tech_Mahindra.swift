09f13d25435285cfe9af09d2a49e96ef22395ac5
narendrajagne <nkjagne1@gmail.com>
2025-03-28 17:16:52 +0530

    added file in Tech Mahindra

--- To-Do-List/To-Do-List/SwiftFundamentals/Tech_Mahindra.swift
+++ To-Do-List/To-Do-List/SwiftFundamentals/Tech_Mahindra.swift
@@ -119,3 +119,4 @@ import UIKit
 // What is SOILD Principle I - Stand For
 // Explain Morden Cocurrency ?
 // Explain of sync Morden Concurrency and Async Morden Concurreny ?
+// What is Virtual Table in Swift ?
